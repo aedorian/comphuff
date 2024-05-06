efc4bc4da799c7544f1694ded73de29a20c5040d
aedorian <141950489+aedorian@users.noreply.github.com>
2024-05-06 09:43:35 +0200

    Delete src directory

--- src/gen_fich.h
+++
@@ -1,7 +0,0 @@
-#ifndef _GEN_FICH_H_
-#define _GEN_FICH_H_
-
-/* nb_car >= 0, nb_car_diff entre 1 et 256, graine >= 0 */
-void gen_fich(int nb_car, int nb_car_diff, int graine, char * nom);
-
-#endif
